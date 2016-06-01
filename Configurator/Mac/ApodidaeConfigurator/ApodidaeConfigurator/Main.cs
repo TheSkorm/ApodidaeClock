832bebd77883242c57e1aa08d3db2a9e62a9df39
Jackson Ming Hu <huming2207@gmail.com>
2016-06-01 16:37:34 +1000

    Initial commit for Mac configurator UI

---
+++ Configurator/Mac/ApodidaeConfigurator/ApodidaeConfigurator/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace ApodidaeConfigurator
+{
+	static class MainClass
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
