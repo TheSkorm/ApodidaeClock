d5391511615fe074c87ad89d8fda2878b7cf51e3
Jackson Ming Hu <huming2207@gmail.com>
2016-06-01 20:14:43 +1000

    Changed my mind, use swift instead of Xamarin/Mono

---
+++ Configurator/Mac/MyApodidae.Mac/MyApodidae/MyApodidae/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyApodidae
+//
+//  Created by MingHu on 16/6/1.
+//  Copyright © 2016年 Jackson Ming Hu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
